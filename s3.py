11e009db69c2d1cd31df31c4318d8943b4beb9dd
Amrit Gill <amgill1234@gmail.com>
2017-12-24 00:07:05 -0500

    Added way to delete all objects in a bucket when deleting a bucket

--- s3.py
+++ s3.py
@@ -28,24 +28,38 @@ def create_bucket(name):
 	exists = bucket_exists(name)
 	if name != None and exists == False:
 		s3.create_bucket(Bucket=name)
-		print ("INFO: Created new s3 bucket: %s" % name)
+		print ("INFO: cdCreated new s3 bucket: %s" % name)
 
 
 def create_folder_in_bucket(bucket_name, folder_name = 'test_bucket'):
 	s3 = boto3.client('s3')
 	if bucket_exists(bucket_name):
 		s3.put_object(Bucket=bucket_name,Key=folder_name+'/')
+		print("INFO: New Object has been placed: %s" % folder_name)
 	else:
 		print ('ERROR: Bucket %s does not exist!!!' % bucket_name)
 
 
 def delete_bucket(name):
 	s3 = boto3.client('s3')
-	if name != None and bucket_exists(name) == True:
-		s3.delete_bucket(Bucket=name)
-		print ("Bucket %s deleted!" % name)
+	buckets = get_all_bucket_names()
+	if bucket_exists(name):
+	 	### clean up the entire mess in that bucket first
+	 	bucket_index = buckets.index(name)
+	 	objs = s3.list_objects(Bucket=buckets[bucket_index])
+	 	if objs.get('Contents') != None:
+	 		print ("INFO: Cleaning up all objects inside the bucket")
+			for each_obj in objs['Contents']:
+				print ("WARN: Deleting %s" % each_obj['Key'])
+				s3.delete_object(Bucket=name, Key=each_obj['Key'])
+		else:
+			print ("SKIP: Bucket is already empty!")
+	 	s3.delete_bucket(Bucket=name)
+	 	print ("INFO: Bucket %s deleted!" % name)
 
+#delete_bucket('amrit-test-boto3-bucket2')
 create_bucket("amrit-test-boto3-bucket2")
-# delete_bucket("amrit-test-boto3-bucket")
-# bucket_exists("amrit-test-boto3-bucket2")
-create_folder_in_bucket("amrit-test-boto3-bucket2")
+for x in range(1,10):
+	create_folder_in_bucket("amrit-test-boto3-bucket2", 'test-folder-'+str(x))
+delete_bucket("amrit-test-boto3-bucket2")
+
