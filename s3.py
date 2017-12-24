34808ecb863c6ee86a14560154bdb44777ec7b44
Amrit Gill <amgill1234@gmail.com>
2017-12-23 23:19:05 -0500

    Added way to add folder in an existing bucket

--- s3.py
+++ s3.py
@@ -1,8 +1,6 @@
 import boto3
-
-
-b_name = 'amrit-test-boto3-bucket'
-
+import os
+import random
 
 def get_all_bucket_names():
 	s3 = boto3.client('s3')
@@ -13,14 +11,15 @@ def get_all_bucket_names():
 
 	return bucket_names
 
+
 def bucket_exists(name):
 	s3 = boto3.client('s3')
 	bucket_names = get_all_bucket_names()
 	if name in bucket_names:
-		print ("INFO: The bucket %s already exists" % name)
+		print ("INFO: The bucket '%s' exists" % name)
 		return True
 	else:
-		print ("INFO: The bucket %s does not exists" % name)
+		print ("INFO: The bucket '%s' does not exists" % name)
 		return False
 
 
@@ -29,7 +28,15 @@ def create_bucket(name):
 	exists = bucket_exists(name)
 	if name != None and exists == False:
 		s3.create_bucket(Bucket=name)
-		print ("Created new s3 bucket: %s" % name)
+		print ("INFO: Created new s3 bucket: %s" % name)
+
+
+def create_folder_in_bucket(bucket_name, folder_name = 'test_bucket'):
+	s3 = boto3.client('s3')
+	if bucket_exists(bucket_name):
+		s3.put_object(Bucket=bucket_name,Key=folder_name+'/')
+	else:
+		print ('ERROR: Bucket %s does not exist!!!' % bucket_name)
 
 
 def delete_bucket(name):
@@ -38,7 +45,7 @@ def delete_bucket(name):
 		s3.delete_bucket(Bucket=name)
 		print ("Bucket %s deleted!" % name)
 
-
-# create_bucket("amrit-test-boto3-bucket2")
-delete_bucket("amrit-test-boto3-bucket2")
+create_bucket("amrit-test-boto3-bucket2")
+# delete_bucket("amrit-test-boto3-bucket")
 # bucket_exists("amrit-test-boto3-bucket2")
+create_folder_in_bucket("amrit-test-boto3-bucket2")
